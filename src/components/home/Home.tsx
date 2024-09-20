81bb7c7cb52cf02ad5242025c02e504965be6862
minumsa <icycyi@naver.com>
2024-09-20 17:30:33 +0900

    feat: 헤더 생성

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,7 +3,13 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1>Landing Page</h1>
+      <header className={styles.header}>
+        <h1 className={styles.header}>
+          <div>최유일</div>
+          <div>프로젝트</div>
+          <div>묻고 답하기</div>
+        </h1>
+      </header>
     </div>
   );
 };
